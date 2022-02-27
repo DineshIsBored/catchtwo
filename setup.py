c90f098d783332838f9e1448bdce3aef164de166
devraza <devraza@outlook.com>
2022-02-27 19:19:22 +0000

    Make replit version up to date with main

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 with open("data/config.json", "a") as file:
-    print("Input your discord authorization token:")
+    print("\nInput your discord authorization token:")
     auth_token = input()
     file.write("{\n")
     file.write(f'   "user_token" : "{auth_token}",\n')
-    print("Now, input the preferred Channel ID for spamming and catching:")
+    print("Now, input the Channel ID of the channel for spamming and catching:")
     channel_id = input()
     file.write(f'   "channel_id" : "{channel_id}"\n')
     file.write("}")
 
-print("Setup completed. Please run the bot using the correct command:\nPlease see the GitHub repo for help on this.")
+print("\n\nSetup completed. Please run the bot using the correct command:\nPlease see the GitHub repo for help on this. However, please try:\npython3 main.py")
